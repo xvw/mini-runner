f1ef8bbcdf1b324684fcd3afa950038a4f907a0d
xvw <xavier.vdw@gmail.com>
2016-11-14 08:32:00 +0100

    Purge useless things

--- js/game.js
+++ js/game.js
@@ -1,3 +1,35 @@
-"use strict";
+(function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else if (typeof exports !== "undefined") {
+        factory();
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory();
+        global.game = mod.exports;
+    }
+})(this, function () {
+    "use strict";
 
-var canvas = document.getElementById("canvas");
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    // A small game writted in JavaScript
+
+    // Canvas element
+    var canvas = document.getElementById("game");
+
+    // Class for the PlayArea
+
+    var Area = function Area() {
+        _classCallCheck(this, Area);
+    };
+
+    // Start the game
+    var game = new Area();
+});
--- src/game.js
+++ src/game.js
@@ -1,1 +1,14 @@
-let canvas = document.getElementById("canvas");
+// A small game writted in JavaScript
+
+// Canvas element
+const canvas = document.getElementById("game");
+
+// Class for the PlayArea
+class Area {
+    constructor() {
+
+    }
+}
+
+// Start the game
+let game = new Area()
