90fef26a3b37eb550c7b70609246c97ff681ae16
xvw <xavier.vdw@gmail.com>
2016-11-15 18:30:37 +0100

    Append some stuff

--- js/game.js
+++ js/game.js
@@ -1,35 +1,281 @@
 (function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-        define([], factory);
-    } else if (typeof exports !== "undefined") {
-        factory();
-    } else {
-        var mod = {
-            exports: {}
-        };
-        factory();
-        global.game = mod.exports;
-    }
+			if (typeof define === "function" && define.amd) {
+						define([], factory);
+			} else if (typeof exports !== "undefined") {
+						factory();
+			} else {
+						var mod = {
+									exports: {}
+						};
+						factory();
+						global.game = mod.exports;
+			}
 })(this, function () {
-    "use strict";
+			'use strict';
 
-    function _classCallCheck(instance, Constructor) {
-        if (!(instance instanceof Constructor)) {
-            throw new TypeError("Cannot call a class as a function");
-        }
-    }
+			function _possibleConstructorReturn(self, call) {
+						if (!self) {
+									throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+						}
 
-    // A small game writted in JavaScript
+						return call && (typeof call === "object" || typeof call === "function") ? call : self;
+			}
 
-    // Canvas element
-    var canvas = document.getElementById("game");
+			function _inherits(subClass, superClass) {
+						if (typeof superClass !== "function" && superClass !== null) {
+									throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+						}
 
-    // Class for the PlayArea
+						subClass.prototype = Object.create(superClass && superClass.prototype, {
+									constructor: {
+												value: subClass,
+												enumerable: false,
+												writable: true,
+												configurable: true
+									}
+						});
+						if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+			}
 
-    var Area = function Area() {
-        _classCallCheck(this, Area);
-    };
+			function _classCallCheck(instance, Constructor) {
+						if (!(instance instanceof Constructor)) {
+									throw new TypeError("Cannot call a class as a function");
+						}
+			}
 
-    // Start the game
-    var game = new Area();
+			var _createClass = function () {
+						function defineProperties(target, props) {
+									for (var i = 0; i < props.length; i++) {
+												var descriptor = props[i];
+												descriptor.enumerable = descriptor.enumerable || false;
+												descriptor.configurable = true;
+												if ("value" in descriptor) descriptor.writable = true;
+												Object.defineProperty(target, descriptor.key, descriptor);
+									}
+						}
+
+						return function (Constructor, protoProps, staticProps) {
+									if (protoProps) defineProperties(Constructor.prototype, protoProps);
+									if (staticProps) defineProperties(Constructor, staticProps);
+									return Constructor;
+						};
+			}();
+
+			// A small game writted in JavaScript
+
+			// Canvas element
+			var width = 720;
+			var height = 200;
+			var canvas = document.getElementById('game');
+			var context = canvas.getContext('2d');
+
+			// Utils
+
+			// Mixins for coordinate element
+
+			var Point = function () {
+						function Point(x, y) {
+									_classCallCheck(this, Point);
+
+									this.x = x;
+									this.y = y;
+						}
+
+						_createClass(Point, [{
+									key: 'getX',
+									value: function getX() {
+												return this.x;
+									}
+						}, {
+									key: 'getY',
+									value: function getY() {
+												return this.y;
+									}
+						}, {
+									key: 'setX',
+									value: function setX(x) {
+												this.x = x;
+									}
+						}, {
+									key: 'setY',
+									value: function setY(y) {
+												this.y = y;
+									}
+						}]);
+
+						return Point;
+			}();
+
+			var Sprite = function (_Point) {
+						_inherits(Sprite, _Point);
+
+						function Sprite() {
+									_classCallCheck(this, Sprite);
+
+									var _this = _possibleConstructorReturn(this, (Sprite.__proto__ || Object.getPrototypeOf(Sprite)).call(this, 0, 0));
+
+									_this.bitmap = null;
+									return _this;
+						}
+
+						// Draw function
+
+
+						_createClass(Sprite, [{
+									key: 'draw',
+									value: function draw(x, y) {
+												if (this.bitmap !== null) {
+															context.save();
+															this.bitmap.draw(x, y);
+															context.restore();
+												}
+									}
+						}]);
+
+						return Sprite;
+			}(Point);
+
+			var Bitmap_fun = function () {
+						function Bitmap_fun(callback) {
+									_classCallCheck(this, Bitmap_fun);
+
+									this.fun = callback;
+						}
+
+						_createClass(Bitmap_fun, [{
+									key: 'draw',
+									value: function draw(x, y) {
+												this.fun(canvas, context, x, y);
+									}
+						}]);
+
+						return Bitmap_fun;
+			}();
+
+			var Rect = function (_Sprite) {
+						_inherits(Rect, _Sprite);
+
+						function Rect(color, w, h) {
+									_classCallCheck(this, Rect);
+
+									var _this2 = _possibleConstructorReturn(this, (Rect.__proto__ || Object.getPrototypeOf(Rect)).call(this));
+
+									_this2.width = w;
+									_this2.height = h;
+									_this2.color = color;
+									var that = _this2;
+									_this2.bitmap = new Bitmap_fun(function (c, ctx, x, y) {
+												ctx.fillStyle = that.color;
+												ctx.fillRect(x, y, w, h);
+									});
+									return _this2;
+						}
+
+						return Rect;
+			}(Sprite);
+
+			var Game_player = function (_Point2) {
+						_inherits(Game_player, _Point2);
+
+						function Game_player() {
+									_classCallCheck(this, Game_player);
+
+									return _possibleConstructorReturn(this, (Game_player.__proto__ || Object.getPrototypeOf(Game_player)).call(this, 20, 160));
+						}
+
+						_createClass(Game_player, [{
+									key: 'update',
+									value: function update() {}
+						}]);
+
+						return Game_player;
+			}(Point);
+
+			var Sprite_player = function (_Sprite2) {
+						_inherits(Sprite_player, _Sprite2);
+
+						function Sprite_player(player) {
+									_classCallCheck(this, Sprite_player);
+
+									var _this4 = _possibleConstructorReturn(this, (Sprite_player.__proto__ || Object.getPrototypeOf(Sprite_player)).call(this));
+
+									_this4.player = player;
+									_this4.bitmap = new Rect('red', 32, 32);
+									return _this4;
+						}
+
+						_createClass(Sprite_player, [{
+									key: 'update',
+									value: function update() {
+												this.player.update();
+												this.x = this.player.getX();
+												this.y = this.player.getY();
+												this.draw(this.x, this.y);
+									}
+						}]);
+
+						return Sprite_player;
+			}(Sprite);
+
+			var Area = function () {
+						function Area() {
+									_classCallCheck(this, Area);
+
+									this.internalClock();
+									this.initializeCanvas();
+									this.player = new Game_player();
+									this.initializeSprites();
+						}
+
+						_createClass(Area, [{
+									key: 'update',
+									value: function update(game) {
+												this.internalClock();
+												context.clearRect(0, 0, width, height);
+												// console.log(this.clock);
+												this.performUpdate();
+												// Buffered Loop
+												window.requestAnimationFrame(function () {
+															game.update(game);
+												});
+									}
+						}, {
+									key: 'performUpdate',
+									value: function performUpdate() {
+												// Update function
+												this.sprite_player.update();
+									}
+						}, {
+									key: 'internalClock',
+									value: function internalClock() {
+												this.clock = new Date();
+									}
+						}, {
+									key: 'initializeCanvas',
+									value: function initializeCanvas() {
+												canvas.width = width;
+												canvas.height = height;
+												canvas.style.width = '' + width + 'px';
+												canvas.style.height = '' + height + 'px';
+									}
+						}, {
+									key: 'initializeSprites',
+									value: function initializeSprites() {
+												this.sprite_player = new Sprite_player(this.player);
+									}
+						}, {
+									key: 'getPlayer',
+									value: function getPlayer() {
+												return this.player;
+									}
+						}]);
+
+						return Area;
+			}();
+
+			// Start the game
+			var game = new Area();
+			window.requestAnimationFrame(function () {
+						game.update(game);
+			});
 });
--- src/game.js
+++ src/game.js
@@ -1,14 +1,152 @@
 // A small game writted in JavaScript
 
 // Canvas element
-const canvas = document.getElementById("game");
+const width   = 720;
+const height  = 200;
+const canvas  = document.getElementById('game');
+const context = canvas.getContext('2d'); 
 
-// Class for the PlayArea
-class Area {
+
+// Utils
+
+// Mixins for coordinate element
+class Point {
+    constructor(x, y) {
+	this.x = x;
+	this.y = y;
+    }
+    getX()  { return this.x; }
+    getY()  { return this.y; }
+    setX(x) { this.x = x;    }
+    setY(y) { this.y = y;    }
+}
+
+// A minimalistic Sprite representation
+class Sprite extends Point {
     constructor() {
-
+	super(0, 0);
+	this.bitmap = null;
+    }
+    
+    // Draw function
+    draw(x, y) {
+	if (this.bitmap !== null) {
+	    context.save();
+	    this.bitmap.draw(x, y);
+	    context.restore();
+	}
     }
 }
 
+// Bitmappable
+// FOR DEBUGGING
+class Bitmap_fun {
+    constructor(callback) {
+	this.fun = callback;
+    }
+    draw(x, y) {
+	this.fun(canvas, context, x, y);
+    }
+    
+}
+
+// Visual Rect
+// FOR DEBUGGING
+class Rect extends Sprite {
+    constructor(color, w, h) {
+	super();
+	this.width  = w;
+	this.height = h;
+	this.color  = color;
+	let that = this;
+	this.bitmap = new Bitmap_fun(function(c, ctx, x, y) {
+	    ctx.fillStyle = that.color;
+	    ctx.fillRect(x ,y, w, h);
+	});
+    }
+}
+
+// Model
+
+// Player
+class Game_player extends Point {
+    constructor() {
+	super(10, 160);
+    }
+    update() {
+    }
+}
+
+// Views
+
+class Sprite_player extends Sprite {
+    constructor(player) {
+	super();
+	this.player = player;
+	this.bitmap = new Rect('red', 32, 32);
+    }
+    update() {
+	this.player.update();
+	this.x = this.player.getX();
+	this.y = this.player.getY();
+	this.draw(this.x, this.y);
+    }
+}
+
+
+
+// Game representation
+class Area {
+    
+    constructor() {
+	this.internalClock();
+	this.initializeCanvas();
+	this.player = new Game_player();
+	this.initializeSprites();
+    }
+
+    update(game) {
+	this.internalClock();
+	context.clearRect(0,0,width, height);
+	// console.log(this.clock);
+	this.performUpdate();
+	// Buffered Loop
+	window.requestAnimationFrame(function() {
+	    game.update(game);
+	});
+
+    }
+
+    performUpdate() {
+	// Update function
+	this.sprite_player.update();
+    }
+
+    internalClock() {
+	this.clock = new Date();
+    }
+
+    initializeCanvas() {
+	canvas.width        = width;
+	canvas.height       = height;
+	canvas.style.width  = ''+width+'px';
+	canvas.style.height = ''+height+'px';
+    }
+
+    initializeSprites() {
+	this.sprite_player = new Sprite_player(this.player);
+    }
+
+    getPlayer() {
+	return this.player;
+    }
+    
+}
+
 // Start the game
-let game = new Area()
+var game = new Area();
+window.requestAnimationFrame(function() {
+    game.update(game);
+});
+
+
